f834f8a23be30b02cfc4f251b081e57679209b24
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2017-03-24 11:35:53 +0000

    bau: allow errors when detecting nuget command
    Pair: WillPa

--- test_csharp.sh
+++ test_csharp.sh
@@ -8,17 +8,20 @@ function cleanup() {
 cd "$(dirname "$0")"
 
 (cd csharp
- type nuget 2&>1 /dev/null
- if [ $? -eq 0 ]; then
-     echo "nuget command found"
-     nuget restore
- else
-     echo "using downloaded nuget.exe"
-     ls -l nuget.exe
-     mono nuget.exe restore
- fi 
- xbuild
- mono ./bin/Debug/lms.exe&
+  set +e
+  type nuget > /dev/null
+  if [ $? -eq 0 ]; then
+      echo "nuget command found"
+      set -e
+      nuget restore
+  else
+      echo "using downloaded nuget.exe"
+      set -e
+      ls -l nuget.exe
+      mono nuget.exe restore
+  fi 
+  xbuild
+  mono ./bin/Debug/lms.exe&
  echo $! > csharp_lms.pid)
 
 trap cleanup EXIT
