cb7de4a91f3b903ab82e299136758cf957fbd379
Richard Towers <richard_towers@yahoo.co.uk>
2017-03-25 16:10:39 +0000

    bau: Add a C# / dotnet core local matching service
    For simplicity / parity with the ruby version this one uses dynamic types.
    It would probably be more familiar if we added static types for the requests
    and responses, but hey-ho.
    
    The most painful bit of this is the "Hack" to overwrite the Content-Type in the
    response so it doesn't contain "charset=utf-8". This definitely shouldn't be a
    requirement for a local matching service (the MSA should be able to handle having
    a charset in the header). We should confirm that there's no requirement to remove
    charset, update the tests to allow it, and then remove the hack.
    
    Authors: @richardtowers

---
+++ csharp/README.md
@@ -0,0 +1,4 @@
+# C# / .NET Core
+
+This is an implementation of a verify local-matching-service in C# using .NET Core.
+
---
+++ test_csharp.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+function cleanup {
+  pid="$(<./csharp/csharp.pid)"
+
+  # dotnet run starts a child process, but neglects to kill it when it receives a SIGTERM
+  # explicitly kill its child processes first:
+  pkill -P $pid
+  kill $pid
+
+  rm -f ./cshrp/csharp.pid
+  rm -f ./csharp/dotnet-run.out
+}
+
+cd "$(dirname "$0")"
+
+(
+cd csharp
+dotnet restore
+dotnet run > dotnet-run.out &
+echo $! > csharp.pid
+until grep -m 1 'Application started' dotnet-run.out; do sleep 0.1; done
+)
+
+trap cleanup EXIT
+
+(
+cd local-matching-service-tests
+mvn test -DMATCHING_URL=http://localhost:5000/csharp/matching-service -DUSER_ACCOUNT_CREATION_URL=http://localhost:5000/csharp/account-creation
+)
+
