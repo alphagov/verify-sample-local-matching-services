663d922887bacbf004de6611e3e9516aaf981c71
Korny Sietsma <korny@sietsma.com>
2017-04-03 08:16:31 +0100

    fixed for new test logic, updated readme, schema not working with tests yet

--- clojure/yada/src/yada_lms/core.clj
+++ clojure/yada/src/yada_lms/core.clj
@@ -14,30 +14,31 @@
 
 (def matching-request-schema (-> schema-file slurp (cheshire/parse-string true) ->prismatic))
 
+(defn cycle3-match [matching-request]
+  (= "goodvalue" (get-in matching-request [:cycle3Dataset :attributes :nino])))
+
+(defn cycle01-match [matching-request]
+  (= "Griffin" (get-in matching-request [:matchingDataset :surnames 0 :value])))
+
 (defn matching-service-fn [ctx]
-  (let [body (get-in ctx [:parameters :body])]
+  (let [matching-request (get-in ctx [:parameters :body])]
     (cond
-      (not= "LEVEL_2" (:levelOfAssurance body)) {:result "no-match"}
-      (= "badvalue" (get-in body [:cycle3Dataset :attributes :nino])) {:result "no-match"}
-      (= "Griffin" (get-in body [:matchingDataset :surnames 0 :value])) {:result "no-match"}
-      :else {:result "match"}
-      )
-    )
-  )
+      (not= "LEVEL_2" (:levelOfAssurance matching-request)) {:result "failure"}
+      (cycle01-match matching-request) {:result "match"}
+      (cycle3-match matching-request) {:result "match"}
+      :else {:result "no-match"})))
 
 (defn account-creation-fn [ctx]
-  (let [body (get-in ctx [:parameters :body])
-        loa (:levelOfAssurance body)]
+  (let [matching-request (get-in ctx [:parameters :body])
+        loa (:levelOfAssurance matching-request)]
     (cond
-      (= "failurePid" (:hashedPid body)) {:result "failure"}
-      :else {:result "success"}
-      )
-    )
-  )
+      (not= "LEVEL_2" (:levelOfAssurance matching-request)) {:result "failure"}
+      (= "failurePid" (:hashedPid matching-request)) {:result "failure"}
+      :else {:result "success"})))
 
 (defn as-json-post-resource [on-post-fn]
   (yada/resource
-    {:methods {:post {:parameters {:body s/Any}
+    {:methods {:post {:parameters {:body s/Any}             ;schema not yet working
                       :consumes   "application/json"
                       :produces   "application/json"
                       :response   on-post-fn}}}))
@@ -54,7 +55,7 @@
                            {:produces "text/plain"
                             :response (fn [_]
                                         (future (Thread/sleep 20) (@closer))
-                                        "shutting down server.")}}})}])
+                                        "shutting down clojure server.")}}})}])
 
 (defn run-server-returning-promise []
   (let [listener (yada/listener (the-routes) {:port 50139})
@@ -68,15 +69,12 @@
 (defn -main
   [& args]
   (let [done (run-server-returning-promise)]
-    (println "server running on port 50139... DELETE \"http://localhost/die\" to kill")
+    (println "clojure server running on port 50139... DELETE \"http://localhost/die\" to kill")
     @done))
 
 (comment "to run in a repl, eval this:"
          (def server-promise (run-server-returning-promise))
-         "then either wait on the promise:"
-         @server-promise
-         "or with a timeout"
-         (deref server-promise 1000 :timeout)
-         "or close it yourself"
-         (@closer)
-         )
+         "(you can deref this promise to wait for server exit)"
+         "then close it with the DELETE hook above,"
+         "or close it by calling the closer:"
+         (@closer))
