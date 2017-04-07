d2dd8b064a46ca9871d64ab6cfdbef3273193adb
SonOfBytes <SonOfBytes@users.noreply.github.co>
2017-04-07 15:28:36 +0100

    fix mono/nancy and use docker

--- test_nancy.sh
+++ test_nancy.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
+
 set -e
 
-function cleanup() {
-    kill "$(<./nancy/nancy_lms.pid)" && rm -f ./nancy/nancy_lms.pid
+lang="nancy"
+base="/${lang}"
+port=9991
+
+function cleanup {
+    docker stop verify_sample_lms_${lang}
+    docker rm -f verify_sample_lms_${lang}
 }
 
-cd "$(dirname "$0")"
-
-(cd nancy
- nuget restore
- xbuild
- mono ./bin/Debug/lms.exe&
- echo $! > nancy_lms.pid)
+docker build -t verify_sample_lms/${lang} ${lang}
+docker run -d --name verify_sample_lms_${lang} -p ${port}:${port} verify_sample_lms/${lang}
 
 trap cleanup EXIT
 
+cd "$(dirname "$0")"
+
+until docker logs verify_sample_lms_${lang} |grep -m 1 'Starting local matching service'; do sleep 0.5; done
+
 (cd local-matching-service-tests
-mvn test -DMATCHING_URL=http://localhost:9991/nancy/matching-service -DUSER_ACCOUNT_CREATION_URL=http://localhost:9991/nancy/account-creation)
+mvn test -DMATCHING_URL=http://localhost:${port}${base}/matching-service -DUSER_ACCOUNT_CREATION_URL=http://localhost:${port}${base}/account-creation)
 
 
 
